2a1eb251fcf7745a9379f25eba95bca7dbee4fc9
KkutaySarii <h.kutay.1@gmail.com>
2023-06-06 22:00:23 +0200

    fixes

--- packages/site/src/pages/index.tsx
+++ packages/site/src/pages/index.tsx
@@ -117,49 +117,6 @@ const Index = () => {
         Welcome to <Span>GreedySnap</Span>
       </Heading>
       <CardContainer>
-        {/* {!state.isFlask && (
-          <Card
-            content={{
-              title: 'Install',
-              description:
-                'Snaps is pre-release software only available in MetaMask Flask, a canary distribution for developers with access to upcoming features.',
-              button: <InstallFlaskButton />,
-            }}
-            fullWidth
-          />
-        )}
-        {!state.installedSnap && (
-          <Card
-            content={{
-              title: 'Connect',
-              description:
-                'Get started by connecting to and installing the example snap.',
-              button: (
-                <ConnectButton
-                  onClick={handleConnectClick}
-                  disabled={!state.isFlask}
-                />
-              ),
-            }}
-            disabled={!state.isFlask}
-          />
-        )}
-        {shouldDisplayReconnectButton(state.installedSnap) && (
-          <Card
-            content={{
-              title: 'Reconnect',
-              description:
-                'While connected to a local running snap this button will always be displayed in order to update the snap if a change is made.',
-              button: (
-                <ReconnectButton
-                  onClick={handleConnectClick}
-                  disabled={!state.installedSnap}
-                />
-              ),
-            }}
-            disabled={!state.installedSnap}
-          />
-        )} */}
         <Card
           content={{
             title: 'Send Transaction',
